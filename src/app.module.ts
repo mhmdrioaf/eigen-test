476e9a049b11a46a8db986249f381498b020c8fb
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-07 19:26:52 +0700

    refactor: delete unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { MemberModule } from './member/member.module';
-import { LibraryModule } from './library/library.module';
 import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [
     BookModule,
     MemberModule,
-    LibraryModule,
     PrismaModule.forRoot({
       isGlobal: true,
     }),
--- src/library/library.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LibraryService } from './library.service';
-import { LibraryController } from './library.controller';
-
-@Module({
-  providers: [LibraryService],
-  controllers: [LibraryController]
-})
-export class LibraryModule {}
