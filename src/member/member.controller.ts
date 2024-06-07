5db702630f95a0d99f5f2415b3f6a76f7ab50dbf
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-07 19:26:15 +0700

    docs(member): initialize the swagger UI documentations for the member endpoints

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,18 +1,83 @@
 import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { MemberService } from './member.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('member')
 @Controller('member')
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
   @HttpCode(HttpStatus.OK)
   @Get()
+  @ApiOperation({
+    summary:
+      'Returns all registered members, with the number of book(s) borrowed',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'Returns all registered members, with the number of book(s) borrowed',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          code: {
+            type: 'string',
+            example: 'M001',
+            description: 'The member code',
+          },
+          name: {
+            type: 'string',
+            example: 'Putri',
+            description: 'The member name',
+          },
+          isPenalized: {
+            type: 'boolean',
+            example: false,
+            description: 'Whether the member is penalized or not',
+          },
+          penalizedAt: {
+            type: 'date',
+            nullable: true,
+            example: null,
+            description: 'The date when the member was penalized',
+          },
+          borrowedBooks: {
+            type: 'number',
+            example: 2,
+            description: 'The number of books borrowed by the member',
+          },
+        },
+      },
+    },
+  })
   async getAllMembers(): Promise<TMemberWithBooks[]> {
     return this.memberService.getAllMembers();
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('populate')
+  @ApiOperation({ summary: 'Populate members data' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Members data populated',
+    schema: {
+      type: 'object',
+      properties: {
+        success: {
+          type: 'boolean',
+          description: 'The request status',
+          example: true,
+        },
+        message: {
+          type: 'string',
+          description: 'The response message',
+          example: 'Members has been successfully populated.',
+        },
+      },
+    },
+  })
   async populateMembers(): Promise<IResponse> {
     return this.memberService.populateMembers();
   }
