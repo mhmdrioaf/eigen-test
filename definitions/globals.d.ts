4c9be19efb75867c7c6457a32dd597c95b65ac51
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-07 17:16:02 +0700

    chore: add BorrowedBooksResponse interface

--- definitions/globals.d.ts
+++ definitions/globals.d.ts
@@ -2,3 +2,19 @@ interface IResponse {
   success: boolean;
   message?: string;
 }
+
+interface IGetBorrowedBooksResponse extends IResponse {
+  member: TBorrowedBookMember;
+  borrowedBooks: TBorrowedBook[];
+}
+
+type TBorrowedBookMember = {
+  memberCode: string;
+  memberName: string;
+};
+
+type TBorrowedBook = {
+  borrowedAt: Data;
+  bookCode: string;
+  bookTitle: string;
+};
