9dc2d0659b0d651eb06277304bbf7af3a968a3de
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-07 17:17:03 +0700

    feat(member): initialize the required member features

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { MemberService } from './member.service';
 
 @Controller('member')
-export class MemberController {}
+export class MemberController {
+  constructor(private readonly memberService: MemberService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  async getAllMembers(): Promise<TMemberWithBooks[]> {
+    return this.memberService.getAllMembers();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('populate')
+  async populateMembers(): Promise<IResponse> {
+    return this.memberService.populateMembers();
+  }
+}
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
+import { MEMBERS } from 'src/mock/member.mock';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(private db: PrismaService) {}
+
+  async getAllMembers(): Promise<TMemberWithBooks[]> {
+    try {
+      const members = await this.db.member.findMany({
+        include: {
+          borrowedBooks: {
+            where: {
+              returnedAt: null,
+            },
+            select: {
+              book: {
+                select: {
+                  code: true,
+                  title: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      const membersData: TMemberWithBooks[] = members.map((member) => ({
+        ...member,
+        borrowedBooks: member.borrowedBooks.length,
+      }));
+
+      return membersData;
+    } catch (error) {
+      console.error('Error fetching members: ', error);
+      throw new Error('There was a problem while fetching members');
+    }
+  }
+
+  async populateMembers(): Promise<IResponse> {
+    try {
+      const members = MEMBERS;
+      await this.db.member.createMany({
+        data: members,
+      });
+
+      return {
+        success: true,
+        message: 'Members populated successfully',
+      };
+    } catch (error) {
+      console.error('Error populating members: ', error);
+      throw new Error('There was a problem while populating members');
+    }
+  }
+}
