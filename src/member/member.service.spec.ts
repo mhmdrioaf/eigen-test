34217dade89891c85251163ac87da31034fd222f
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-08 08:15:02 +0700

    test(member): initialize the member service testing

--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MemberService } from './member.service';
+import { PrismaModule } from 'nestjs-prisma';
 
 describe('MemberService', () => {
   let service: MemberService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [MemberService],
     }).compile();
 
@@ -14,5 +16,27 @@ describe('MemberService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllMembers', () => {
+    it('should return an array of members with the number of borrowed books', async () => {
+      await service.getAllMembers().then((res) => {
+        expect(res).toBeInstanceOf(Array);
+        expect(res[0]).toHaveProperty('code');
+        expect(res[0]).toHaveProperty('name');
+        expect(res[0]).toHaveProperty('isPenalized');
+        expect(res[0]).toHaveProperty('penalizedAt');
+        expect(res[0]).toHaveProperty('borrowedBooks');
+      });
+    });
+  });
+
+  describe('populateMembers', () => {
+    it('should return the success message', async () => {
+      await service.populateMembers().then((res) => {
+        expect(res.success).toBe(true);
+        expect(res.message).toBe('Members populated successfully');
+      });
+    });
   });
 });
