c8d4632e26a318f99898ad3c3555714114ef8a0d
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-06-08 08:14:25 +0700

    chore: add the members initial data

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
-import { MEMBERS } from 'src/mock/member.mock';
 
 @Injectable()
 export class MemberService {
   constructor(private db: PrismaService) {}
+
+  MEMBERS: TMember[] = [
+    {
+      code: 'M001',
+      name: 'Angga',
+      isPenalized: true,
+      penalizedAt: new Date('2024-06-07T10:25:09.230Z'),
+    },
+    {
+      code: 'M002',
+      name: 'Ferry',
+      isPenalized: false,
+      penalizedAt: null,
+    },
+    {
+      code: 'M003',
+      name: 'Putri',
+      isPenalized: false,
+      penalizedAt: null,
+    },
+  ];
 
   async getAllMembers(): Promise<TMemberWithBooks[]> {
     try {
@@ -40,7 +60,7 @@ export class MemberService {
 
   async populateMembers(): Promise<IResponse> {
     try {
-      const members = MEMBERS;
+      const members = this.MEMBERS;
       await this.db.member.createMany({
         data: members,
         skipDuplicates: true,
